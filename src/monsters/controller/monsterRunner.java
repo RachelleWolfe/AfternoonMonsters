ab754a13f10749f80146bdc0f740f6cc9f5c74d2
RachelleWolfe <rachelgoodliffe@hotmail.com>
2014-09-15 13:29:41 -0600

    Add to repo
    Add to repo

---
+++ src/monsters/controller/monsterRunner.java
@@ -0,0 +1,10 @@
+package monsters.controller;
+
+public class monsterRunner {
+
+	public static void main (String [] args)
+	{
+		MonsterAppController appController = new MonsterAppController();
+		appController.start();
+	}
+}
